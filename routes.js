fec42a773a580de1b69a5d30aca5943615ff2e49
Brenda02112006 <139500962+Brenda02112006@users.noreply.github.com>
2024-03-26 16:06:15 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const { renderHomePage, searchBooks } = require('./controllers');
 module.exports = function (app) {
   // Rota para a página inicial
   app.get('/', (req, res) => {
-    renderHomePage(req, res); // Aqui está ocorrendo o erro
+    renderHomePage(req, res); 
   });
 
   // Rota para realizar buscas
